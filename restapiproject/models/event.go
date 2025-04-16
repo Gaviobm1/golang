94e02edf0b70edb908f2cf30bb2b3ec972ab04d7
gaviobm1 <142741214+Gaviobm1@users.noreply.github.com>
2025-04-16 12:29:55 +0200

    Get by id implemented

--- restapiproject/models/event.go
+++ restapiproject/models/event.go
@@ -13,8 +13,6 @@ type Event struct {
 	UserID                      int
 }
 
-var events = []Event{}
-
 func (e Event) Save() error {
 	query := `
 	INSERT INTO events(name, description, location, dateTime, user_id)
@@ -57,4 +55,26 @@ func GetAllEvents() ([]Event, error) {
 		events = append(events, event)
 	}
 	return events, nil
+}
+
+func GetEventById(id int64) (*Event, error) {
+	query := "SELECT * FROM events WHERE id = ?"
+	row := db.DB.QueryRow(query, id)
+
+	var event Event
+
+	err := row.Scan(
+		&event.ID,
+		&event.Name,
+		&event.Description,
+		&event.Location,
+		&event.DateTime,
+		&event.UserID,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &event, err
 }
